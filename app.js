6ed310a7add40d306deca8a6df84e2a3860e4437
Michael Mašek <michael.masek@me.com>
2017-06-09 00:25:55 +0200

    First

---
+++ app.js
@@ -0,0 +1,51 @@
+(function (app) {
+    "use strict";
+    app.controller('AppController', function ($scope, $q, $http) {
+
+        $scope.weapon = {
+            basicDmgUp: null,
+            basicDmgDown: null,
+            increasedWeaponDamage: null,
+            damageValue: null,
+            increasedDamage: null,
+            ratings: []
+        }
+        $scope.weapon = JSON.parse(localStorage.getItem('app.weapon'));
+
+        $scope.save = function() {
+        localStorage.setItem('app.weapon', JSON.stringify($scope.weapon));
+        }
+
+        $scope.getDamage = function () {
+            return $scope.weapon.basicDmgDown + $scope.weapon.basicDmgUp;
+        };
+
+        $scope.getIncreasedWeaponDamage = function () {
+            return $scope.weapon.increasedWeaponDamage;
+        };
+
+        $scope.getDamageValue = function () {
+            return $scope.weapon.damageValue;
+        };
+        $scope.getIncreasedDamage = function () {
+            return $scope.weapon.increasedDamage;
+        };
+
+        $scope.getDamageDownWithFixDmgAndGems = function () {
+            return $scope.weapon.basicDmgDown + $scope.weapon.damageValue;
+        }
+
+        $scope.getValue = function () {
+            return parseInt($scope.weapon.ratings[0]) + parseInt($scope.weapon.ratings[1]) + parseInt($scope.weapon.ratings[2]) + parseInt($scope.weapon.ratings[3]) + parseInt($scope.weapon.ratings[4]);
+        };
+
+        $scope.getValueSum = function () {
+            return $scope.weapon.ratings.reduce(function (a, b) {
+                return parseInt(a, 10) + parseInt(b, 10);
+            }, 0);
+        };
+
+
+    });
+
+})(angular.module('app', []));
